1e6eb69a8d7aafeac66e380bfd6ecff749b6d29b
sue445 <sue445@sue445.net>
2024-07-22 22:08:31 +0900

    Fixed cannot use ruby.RbDefineModule("Dummy") (value of type ruby._Ctype_ulong) as _Ctype_ulong value in assignment
    ```
    Error: ./dummy.go:16:14: cannot use ruby.RbDefineModule("Dummy") (value of type ruby._Ctype_ulong) as _Ctype_ulong value in assignment
    ```

--- testdata/dummy/ext/dummy/dummy.go
+++ testdata/dummy/ext/dummy/dummy.go
@@ -1,15 +1,12 @@
 package main
 
-/*
-#include "dummy.h"
-*/
 import "C"
 
 import (
 	"github.com/sue445/go-gem-wrapper"
 )
 
-var rb_mDummy C.VALUE
+var rb_mDummy ruby.VALUE
 
 //export Init_dummy
 func Init_dummy() {
--- wrapper.go
+++ wrapper.go
@@ -9,12 +9,14 @@ import (
 	"unsafe"
 )
 
+type VALUE C.VALUE
+
 func GOSTRING_PTR(str string) *C.char {
 	bytes := append([]byte(str), '\000')
 
 	return (*C.char)(unsafe.Pointer(&bytes[0]))
 }
 
-func RbDefineModule(name string) C.VALUE {
-	return C.rb_define_module(GOSTRING_PTR(name))
+func RbDefineModule(name string) VALUE {
+	return (VALUE)(C.rb_define_module(GOSTRING_PTR(name)))
 }
