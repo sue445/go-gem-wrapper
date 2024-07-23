3c0d7c035c6a594df23634723f1b5fecf1489d76
sue445 <sue445@sue445.net>
2024-07-24 08:59:52 +0900

    Move to doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package ruby is a wrapper for creating native Ruby extension in Go
+//
+// ref.
+//   - https://docs.ruby-lang.org/en/master/extension_rdoc.html
+//   - https://docs.ruby-lang.org/ja/latest/function/index.html
+package ruby
