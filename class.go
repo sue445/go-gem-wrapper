32c892f58b5f4d80bfcda067cf2a1c5a785f21d5
sue445 <sue445@sue445.net>
2024-07-24 08:48:38 +0900

    Move to module.go

--- class.go
+++ class.go
@@ -9,11 +9,6 @@ import (
 	"unsafe"
 )
 
-// RbDefineModule calls `rb_define_module` in C
-func RbDefineModule(name string) VALUE {
-	return VALUE(C.rb_define_module(goString2Char(name)))
-}
-
 // RbDefineSingletonMethod calls `rb_define_singleton_method` in C
 func RbDefineSingletonMethod(klass VALUE, name string, fun unsafe.Pointer, args int) {
 	cname := goString2Char(name)
