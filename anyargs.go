6ef6e05db3cd6e469c157beaa5ad211c36cc0542
sue445 <sue445@sue445.net>
2024-07-24 08:49:17 +0900

    Add link

--- anyargs.go
+++ anyargs.go
@@ -9,6 +9,8 @@ import (
 	"unsafe"
 )
 
+// c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/anyargs.h
+
 // RbDefineSingletonMethod calls `rb_define_singleton_method` in C
 func RbDefineSingletonMethod(klass VALUE, name string, fun unsafe.Pointer, args int) {
 	cname := goString2Char(name)
--- module.go
+++ module.go
@@ -5,6 +5,8 @@ package ruby
 */
 import "C"
 
+// c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/module.h
+
 // RbDefineModule calls `rb_define_module` in C
 func RbDefineModule(name string) VALUE {
 	return VALUE(C.rb_define_module(goString2Char(name)))
