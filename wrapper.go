604de52343c17ca6c8094a9cd0f4918ea85ae49d
sue445 <sue445@sue445.net>
2024-07-25 00:31:52 +0900

    Tweak String2Char and string2Char

--- wrapper.go
+++ wrapper.go
@@ -9,15 +9,16 @@ import (
 	"unsafe"
 )
 
+// String2Char convert from Go string to `*Char`
+func String2Char(str string) *Char {
+	return (*Char)(string2Char(str))
+}
+
+// string2Char convert from Go string to `*C.char`
 func string2Char(str string) *C.char {
 	bytes := append([]byte(str), '\000')
 
 	return (*C.char)(unsafe.Pointer(&bytes[0]))
-}
-
-// String2Char convert from Go string to `C.char`
-func String2Char(str string) *Char {
-	return (*Char)(string2Char(str))
 }
 
 // Value2String convert from `VALUE` to Go string
