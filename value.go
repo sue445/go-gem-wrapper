34b57247e67ac83267817a6ae55197faf5fcab4e
sue445 <sue445@sue445.net>
2024-07-25 08:20:04 +0900

    Tweak comment

--- value.go
+++ value.go
@@ -7,5 +7,5 @@ import "C"
 
 // c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/value.h
 
-// VALUE represents `VALUE` in C
+// VALUE is a type for passing `C.VALUE` in and out of package
 type VALUE C.VALUE
