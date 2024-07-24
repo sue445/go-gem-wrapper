0fdec735a027c080caef8d6d967abfffb12d167a
sue445 <sue445@sue445.net>
2024-07-24 23:38:20 +0900

    Impl Value2String, StringLen

--- wrapper.go
+++ wrapper.go
@@ -19,3 +19,23 @@ func string2Char(str string) *C.char {
 func String2Char(str string) *Char {
 	return (*Char)(string2Char(str))
 }
+
+// Value2String convert from `VALUE` to Go string
+func Value2String(str VALUE) string {
+	return value2String(C.VALUE(str))
+}
+
+// value2String convert from `C.VALUE` to Go string
+func value2String(str C.VALUE) string {
+	return C.GoStringN(rstringPtr(str), rstringLenint(str))
+}
+
+// StringLen returns string length as `Long`
+func StringLen(str string) Long {
+	return Long(stringLen(str))
+}
+
+// stringLen returns string length as `C.long`
+func stringLen(str string) C.long {
+	return C.long(len(str))
+}
