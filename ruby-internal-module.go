c628a94d915e71d4152f764603dc2a2a09a52495
sue445 <sue445@sue445.net>
2024-07-28 01:05:08 +0900

    Add RbDefineClassUnder

--- ruby-internal-module.go
+++ ruby-internal-module.go
@@ -15,3 +15,12 @@ import "C"
 func RbDefineModule(name string) VALUE {
 	return VALUE(C.rb_define_module(string2Char(name)))
 }
+
+// RbDefineClassUnder calls `rb_define_class_under` in C
+//
+// Original definition is following
+//
+//	VALUE rb_define_class_under(VALUE outer, const char *name, VALUE super)
+func RbDefineClassUnder(outer VALUE, name string, super VALUE) VALUE {
+	return VALUE(C.rb_define_class_under(C.VALUE(outer), string2Char(name), C.VALUE(super)))
+}
