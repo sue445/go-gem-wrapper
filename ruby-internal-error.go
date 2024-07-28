9755350746473032d64feaf4e82fecfe44b4307b
sue445 <sue445@sue445.net>
2024-07-28 09:13:34 +0900

    Write RbRaise doc

--- ruby-internal-error.go
+++ ruby-internal-error.go
@@ -18,6 +18,14 @@ import (
 // c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/error.h
 
 // RbRaise calls `rb_raise` in C
+//
+// Original definition is following
+//
+//	void rb_raise(VALUE exc, const char *fmt, ...)
+//
+// `format` is supported in [Go's format], not Ruby
+//
+// [Go's format]: https://pkg.go.dev/fmt
 func RbRaise(exc VALUE, format string, a ...interface{}) {
 	str := fmt.Sprintf(format, a...)
 	C.rb_raise2(C.VALUE(exc), string2Char(str))
