36443277f12a4564ba89b17d1723bd1b8701c4d4
sue445 <sue445@sue445.net>
2024-07-28 23:57:22 +0900

    Add RbIntern, ID

---
+++ ruby-internal-symbol.go
@@ -0,0 +1,17 @@
+package ruby
+
+/*
+#include "ruby.h"
+*/
+import "C"
+
+// c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/symbol.h
+
+// RbIntern calls `rb_intern` in C
+//
+// Original definition is following
+//
+//	ID rb_intern(const char *name)
+func RbIntern(name string) ID {
+	return ID(C.rb_intern(string2Char(name)))
+}
--- ruby-internal-value.go
+++ ruby-internal-value.go
@@ -9,3 +9,6 @@ import "C"
 
 // VALUE is a type for passing `C.VALUE` in and out of package
 type VALUE C.VALUE
+
+// ID is a type for passing `C.ID` in and out of package
+type ID C.ID
