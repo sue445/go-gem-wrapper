fde5b20fe612c65dcc68c267d45f57ea39bf6a9b
sue445 <sue445@sue445.net>
2024-07-29 23:09:43 +0900

    Add INT2NUM, NUM2INT

--- ruby-internal-arithmetic-int.go
+++ ruby-internal-arithmetic-int.go
@@ -16,6 +16,11 @@ func RbNum2intInline(x VALUE) Int {
 	return Int(C.rb_num2int_inline(C.VALUE(x)))
 }
 
+// NUM2INT is alias to [RbNum2intInline]
+func NUM2INT(x VALUE) Int {
+	return RbNum2intInline(x)
+}
+
 // RbInt2numInline calls `rb_int2num_inline` in C
 //
 // Original definition is following
@@ -23,4 +28,9 @@ func RbNum2intInline(x VALUE) Int {
 //	VALUE rb_int2num_inline(int v)
 func RbInt2numInline(v Int) VALUE {
 	return VALUE(C.rb_int2num_inline(C.int(v)))
+}
+
+// INT2NUM is alias to [RbInt2numInline]
+func INT2NUM(v Int) VALUE {
+	return RbInt2numInline(v)
 }
