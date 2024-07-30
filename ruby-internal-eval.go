e62074901aec460bda2505fcdbd9ac6d99ebba0e
sue445 <sue445@sue445.net>
2024-07-30 22:49:15 +0900

    Add RbFuncallv

---
+++ ruby-internal-eval.go
@@ -0,0 +1,28 @@
+package ruby
+
+/*
+#include "ruby.h"
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+// c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/eval.h
+
+// RbFuncallv calls `rb_funcallv` in C
+//
+// Original definition is following
+//
+//	VALUE rb_funcallv(VALUE recv, ID mid, int argc, const VALUE *argv)
+func RbFuncallv(recv VALUE, mid ID, argc int, argv []VALUE) VALUE {
+	var cArgs *C.VALUE
+	if argc == 0 {
+		cArgs = (*C.VALUE)(unsafe.Pointer(nil))
+	} else {
+		cArgs = (*C.VALUE)(unsafe.Pointer(&argv[0]))
+	}
+
+	return VALUE(C.rb_funcallv(C.VALUE(recv), C.ID(mid), C.int(argc), cArgs))
+}
