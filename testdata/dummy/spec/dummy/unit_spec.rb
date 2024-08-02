732be084bd48e261a70ef39dcf4f5239a2d19816
sue445 <sue445@sue445.net>
2024-08-03 00:28:20 +0900

    Add spec and rbs

--- testdata/dummy/spec/dummy/unit_spec.rb
+++ testdata/dummy/spec/dummy/unit_spec.rb
@@ -7,4 +7,14 @@ RSpec.describe Dummy::Unit do
       expect(unit.kilobyte).to eq 2048
     end
   end
+
+  describe "#increment" do
+    it "works" do
+      unit = Dummy::Unit.new(1)
+      actual = unit.increment
+
+      expect(actual).to eq 2
+      expect(unit.source).to eq 2
+    end
+  end
 end
