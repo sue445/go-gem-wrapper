712661b677dedd7b621ada0126be417b72be1eb2
sue445 <sue445@sue445.net>
2024-08-04 17:30:44 +0900

    Move all definions to dummy.h

--- testdata/dummy/ext/dummy/dummy.go
+++ testdata/dummy/ext/dummy/dummy.go
@@ -2,15 +2,6 @@ package main
 
 /*
 #include "dummy.h"
-
-VALUE rb_dummy_sum(VALUE self, VALUE a, VALUE b);
-VALUE rb_dummy_with_block(VALUE self, VALUE arg);
-VALUE rb_dummy_with_block2(VALUE self, VALUE arg);
-VALUE rb_dummy_hello(VALUE self, VALUE name);
-VALUE rb_dummy_round_num2(VALUE self, VALUE num, VALUE ndigits);
-VALUE rb_dummy_round_num3(VALUE self, VALUE num, VALUE ndigits);
-VALUE rb_dummy_to_string(VALUE self, VALUE source);
-VALUE rb_dummy_max(VALUE self, VALUE a, VALUE b);
 */
 import "C"
 
