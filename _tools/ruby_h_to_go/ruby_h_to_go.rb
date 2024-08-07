8a1b2f7026ec9e6d01ef154fdc76ee0b4881e7ed
sue445 <sue445@sue445.net>
2024-08-07 09:46:55 +0900

    Tweak var name format

--- _tools/ruby_h_to_go/ruby_h_to_go.rb
+++ _tools/ruby_h_to_go/ruby_h_to_go.rb
@@ -155,8 +155,8 @@ def generate_go_file(definition:, header_dir:)
   casted_go_args = []
   definition[:args].each do |c_arg|
     if c_arg[:type] == "char*"
-      char_var_name = "#{c_arg[:name]}Char"
-      clean_var_name = "#{c_arg[:name]}CharClean"
+      char_var_name = "char#{snake_to_camel(c_arg[:name])}"
+      clean_var_name = "cleanChar#{(c_arg[:name])}"
 
       go_function_lines << "#{char_var_name}, #{clean_var_name} := string2Char(#{c_arg[:name]})"
       go_function_lines << "defer #{clean_var_name}()"
