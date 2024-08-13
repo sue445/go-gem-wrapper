736902a291f8ebc956e130c093f61c5f0d904a0f
sue445 <sue445@sue445.net>
2024-08-14 00:54:14 +0900

    rm -rf testdata/dummy/spec/

--- testdata/dummy/spec/dummy/test_rb_define_module_under_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe "Dummy::TestRbDefineModuleUnder" do
-  it "should be defined" do
-    expect(Dummy::TestRbDefineModuleUnder).to be_a Module
-  end
-end
