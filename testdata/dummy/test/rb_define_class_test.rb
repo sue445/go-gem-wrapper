70136378b2c75a7be9277f1a4b6b03088f2cb946
Go Sueyoshi <sue445@users.noreply.github.com>
2024-08-14 02:07:59 +0900

    Fix test class name

--- testdata/dummy/test/rb_define_class_test.rb
+++ testdata/dummy/test/rb_define_class_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RbDefineClassUnderTest < Test::Unit::TestCase
+class RbDefineClassTest < Test::Unit::TestCase
   test "RbDefineClass" do
     assert { TestRbDefineClass.is_a?(Class) }
   end
