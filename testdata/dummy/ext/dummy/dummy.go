a2e56ae69a2c54e3ed9c6196897594910591948e
sue445 <sue445@sue445.net>
2024-08-18 01:55:50 +0900

    Add go_struct_alloc

--- testdata/dummy/ext/dummy/dummy.go
+++ testdata/dummy/ext/dummy/dummy.go
@@ -7,11 +7,13 @@ VALUE rb_dummy_sum(VALUE self, VALUE a, VALUE b);
 VALUE rb_dummy_hello(VALUE self, VALUE name);
 VALUE rb_dummy_to_string(VALUE self, VALUE source);
 VALUE rb_dummy_max(VALUE self, VALUE a, VALUE b);
+VALUE go_struct_alloc(VALUE klass);
 */
 import "C"
 
 import (
 	ruby "github.com/sue445/go-gem-wrapper"
+	"unsafe"
 )
 
 //export rb_dummy_sum
@@ -51,6 +53,18 @@ func rb_dummy_max(_ C.VALUE, a C.VALUE, b C.VALUE) C.VALUE {
 	return C.VALUE(ruby.INT2NUM(bLong))
 }
 
+// GoStruct is internal reality of Ruby `Dummy::GoStruct`
+type GoStruct struct {
+	x int
+	y int
+}
+
+//export go_struct_alloc
+func go_struct_alloc(klass C.VALUE) C.VALUE {
+	data := GoStruct{}
+	return C.VALUE(ruby.NewGoStruct(ruby.VALUE(klass), unsafe.Pointer(&data)))
+}
+
 //export Init_dummy
 func Init_dummy() {
 	rb_mDummy := ruby.RbDefineModule("Dummy")
@@ -68,6 +82,11 @@ func Init_dummy() {
 
 	// Create OuterClass class
 	ruby.RbDefineClass("TestRbDefineClass", ruby.VALUE(C.rb_cObject))
+
+	// Create Dummy::GoStruct class
+	rb_cGoStruct := ruby.RbDefineClassUnder(rb_mDummy, "GoStruct", ruby.VALUE(C.rb_cObject))
+
+	ruby.RbDefineAllocFunc(rb_cGoStruct, C.go_struct_alloc)
 
 	defineMethodsToDummyTests(rb_mDummy)
 }
