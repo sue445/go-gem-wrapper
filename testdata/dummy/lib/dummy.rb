728f0d13369f3b802434f43723b6a58ca2a32808
sue445 <sue445@sue445.net>
2024-08-18 01:30:06 +0900

    Add GoStruct

--- testdata/dummy/lib/dummy.rb
+++ testdata/dummy/lib/dummy.rb
@@ -3,6 +3,7 @@
 require_relative "dummy/version"
 require_relative "dummy/dummy"
 require_relative "dummy/tests"
+require_relative "dummy/go_struct"
 
 module Dummy
   class Error < StandardError; end
---
+++ testdata/dummy/lib/dummy/go_struct.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Dummy
+  class GoStruct # rubocop:disable Lint/EmptyClass
+  end
+end
