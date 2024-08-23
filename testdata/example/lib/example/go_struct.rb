1e5aa8cd639b6b7c54acf93a86b120d53561f761
sue445 <sue445@sue445.net>
2024-08-23 23:55:39 +0900

    Add comment [ci skip]

--- testdata/example/lib/example/go_struct.rb
+++ testdata/example/lib/example/go_struct.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module Example
-  class GoStruct # rubocop:disable Lint/EmptyClass
+  class GoStruct # rubocop:disable Lint/EmptyClass Methods are defined in ext/example/go_struct.go
   end
 end
