56d41ca5f363115d3230399841154ab2f6ec4a01
sue445 <sue445@sue445.net>
2024-08-23 23:49:37 +0900

    Add `Example::Benchmark` module

--- testdata/example/lib/example.rb
+++ testdata/example/lib/example.rb
@@ -4,6 +4,7 @@ require_relative "example/version"
 require_relative "example/example"
 require_relative "example/tests"
 require_relative "example/go_struct"
+require_relative "example/benchmark"
 
 module Example
   class Error < StandardError; end
