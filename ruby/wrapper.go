9a99d7d792dead3a4e3284d3558d184c7f1d0f14
sue445 <sue445@sue445.net>
2024-08-30 01:59:32 +0900

    Add wrapper

---
+++ ruby/wrapper.go
@@ -0,0 +1,37 @@
+package ruby
+
+/*
+#include "ruby.h"
+*/
+import "C"
+
+// Value2String convert from [VALUE] to Go string
+func Value2String(str VALUE) string {
+	return C.GoStringN(RSTRING_PTR(str), RSTRING_LENINT(str))
+}
+
+// String2Value convert from Go string to [VALUE]
+func String2Value(str string) VALUE {
+	return VALUE(string2Value(str))
+}
+
+// string2Value convert from Go string to `C.VALUE`. (for internal use within package)
+func string2Value(str string) C.VALUE {
+	return RbUtf8StrNew(str, int64(len(str)))
+}
+
+// CallFunction calls receiver's method. (wrapper for [RbFuncallv])
+func CallFunction(receiver VALUE, methodName string, args ...VALUE) VALUE {
+	return RbFuncallv(receiver, RbIntern(methodName), int32(len(args)), args)
+}
+
+// Slice2rbAry convert from Go slice to rb_ary
+func Slice2rbAry(slice []VALUE) VALUE {
+	rbAry := RbAryNewCapa(int64(len(slice)))
+
+	for _, v := range slice {
+		RbAryPush(rbAry, v)
+	}
+
+	return rbAry
+}
