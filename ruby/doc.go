8171f68842f51234829063c375bf65617fbf829b
sue445 <sue445@sue445.net>
2024-08-29 22:17:53 +0900

    Remove legacy wrapper files

--- ruby/doc.go
+++
@@ -1,6 +0,0 @@
-// Package ruby is a wrapper for creating Ruby native extension in Go
-//
-// ref.
-//   - https://docs.ruby-lang.org/en/master/extension_rdoc.html
-//   - https://docs.ruby-lang.org/ja/latest/function/index.html
-package ruby
