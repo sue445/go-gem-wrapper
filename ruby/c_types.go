6f2780e04f46158bb87438e05ebced32f101d114
sue445 <sue445@sue445.net>
2024-09-09 00:26:56 +0900

    Add c types

--- ruby/c_types.go
+++ ruby/c_types.go
@@ -13,3 +13,21 @@ type Long C.long
 
 // Int is a type for passing `C.int` in and out of package
 type Int C.int
+
+// Double is a type for passing `C.double` in and out of package
+type Double C.double
+
+// Short is a type for passing `C.short` in and out of package
+type Short C.short
+
+// IntptrT is a type for passing `C.intptr_t` in and out of package
+type IntptrT C.intptr_t
+
+// Uint32T is a type for passing `C.uint32_t` in and out of package
+type Uint32T C.uint32_t
+
+// UintptrT is a type for passing `C.uintptr_t` in and out of package
+type UintptrT C.uintptr_t
+
+// SizeT is a type for passing `C.size_t` in and out of package
+type SizeT C.size_t
