05f172f83e3eae5a42c5eb487085b6755d6dacdb
sue445 <sue445@sue445.net>
2024-09-15 23:13:18 +0900

    Fix Metrics in Generator#perform

--- _tools/ruby_h_to_go/ruby_h_to_go.rb
+++ _tools/ruby_h_to_go/ruby_h_to_go.rb
@@ -29,38 +29,58 @@ class Generator # rubocop:disable Style/Documentation
   end
 
   def perform
-    parser = RubyHeaderParser::Parser.new(header_dir)
+    clean_generated_files
 
-    function_definitions = parser.extract_function_definitions.map { |d| RubyHToGo::FunctionDefinition.new(d) }
-    struct_definitions   = parser.extract_struct_definitions.map { |d| RubyHToGo::StructDefinition.new(d) }
-    type_definitions     = parser.extract_type_definitions.map { |d| RubyHToGo::TypeDefinition.new(d) }
-
-    # Clean all generated files in dist/
-    FileUtils.rm_f(Dir.glob(File.join(dist_dir, "*.go")))
-
-    type_definitions.each do |definition|
-      definition.write_go_file(dist_dir:, header_dir:)
-    end
-
-    struct_definitions.each do |definition|
-      definition.write_go_file(dist_dir:, header_dir:)
-    end
-
-    function_definitions.each do |definition|
-      definition.write_go_file(dist_dir:, header_dir:)
-    end
+    write_type_definitions_to_go_file
+    write_struct_definitions_to_go_file
+    write_function_definitions_to_go_file
 
     FileUtils.cp(File.join(__dir__, "..", "..", "ruby", "c_types.go"), dist_dir)
 
-    Dir.chdir(File.join(__dir__, "dist")) do
-      system("go fmt", exception: true)
-    end
+    go_fmt
   end
 
   private
 
+  # @return [RubyHeaderParser::Parser]
+  def parser
+    @parser ||= RubyHeaderParser::Parser.new(header_dir)
+  end
+
+  def write_type_definitions_to_go_file
+    type_definitions = parser.extract_type_definitions.map { |d| RubyHToGo::TypeDefinition.new(d) }
+    type_definitions.each do |definition|
+      definition.write_go_file(dist_dir:, header_dir:)
+    end
+  end
+
+  def write_struct_definitions_to_go_file
+    struct_definitions = parser.extract_struct_definitions.map { |d| RubyHToGo::StructDefinition.new(d) }
+    struct_definitions.each do |definition|
+      definition.write_go_file(dist_dir:, header_dir:)
+    end
+  end
+
+  def write_function_definitions_to_go_file
+    function_definitions = parser.extract_function_definitions.map { |d| RubyHToGo::FunctionDefinition.new(d) }
+    function_definitions.each do |definition|
+      definition.write_go_file(dist_dir:, header_dir:)
+    end
+  end
+
   def dist_dir
     File.join(__dir__, "dist")
+  end
+
+  # Clean all generated files in dist/
+  def clean_generated_files
+    FileUtils.rm_f(Dir.glob(File.join(dist_dir, "*.go")))
+  end
+
+  def go_fmt
+    Dir.chdir(dist_dir) do
+      system("go fmt", exception: true)
+    end
   end
 end
 
