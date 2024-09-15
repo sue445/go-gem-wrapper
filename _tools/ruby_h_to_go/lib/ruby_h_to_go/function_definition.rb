317e1b568f687f047320d2223ca1e5a5b113c226
sue445 <sue445@sue445.net>
2024-09-15 22:57:34 +0900

    :cop: rubocop --autocorrect-all --only Style/IdenticalConditionalBranches

--- _tools/ruby_h_to_go/lib/ruby_h_to_go/function_definition.rb
+++ _tools/ruby_h_to_go/lib/ruby_h_to_go/function_definition.rb
@@ -95,12 +95,11 @@ module RubyHToGo
       call_c_method << casted_go_args.join(", ")
       call_c_method << ")"
 
-      if go_function_typeref == ""
-        go_function_lines.push(*before_call_function_lines)
+      go_function_lines.push(*before_call_function_lines)
+if go_function_typeref == ""
         go_function_lines << call_c_method
         go_function_lines.push(*after_call_function_lines)
       else
-        go_function_lines.push(*before_call_function_lines)
         go_function_lines << "ret := #{go_function_typeref}(#{call_c_method})"
         go_function_lines.push(*after_call_function_lines)
         go_function_lines << "return ret"
