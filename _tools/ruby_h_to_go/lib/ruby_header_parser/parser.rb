c1e41625ac2afe648c2633fb0217bc8884938662
sue445 <sue445@sue445.net>
2024-09-18 01:07:50 +0900

    :cop: rubocop --autocorrect --only Style/PerlBackrefs

--- _tools/ruby_h_to_go/lib/ruby_header_parser/parser.rb
+++ _tools/ruby_h_to_go/lib/ruby_header_parser/parser.rb
@@ -208,7 +208,7 @@ module RubyHeaderParser
 
           if parts[-1] =~ /\[([0-9]+)\]$/
             parts[-1].gsub!(/\[([0-9]+)\]$/, "")
-            length = $1.to_i
+            length = ::Regexp.last_match(1).to_i
             pointer = :array
           end
 
