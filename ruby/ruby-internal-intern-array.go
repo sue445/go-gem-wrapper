beebd6bf9c43491e72967dfc4e654394b60d5e7e
sue445 <sue445@sue445.net>
2024-09-22 16:48:59 +0900

    Remove duplicated functions and types

--- ruby/ruby-internal-intern-array.go
+++ ruby/ruby-internal-intern-array.go
@@ -7,61 +7,7 @@ import "C"
 
 // c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/intern/array.h
 
-// RbAryNew calls `rb_ary_new` in C
-//
-// Original definition is following
-//
-//	VALUE rb_ary_new(void)
-func RbAryNew() VALUE {
-	return VALUE(C.rb_ary_new())
-}
-
-// RbAryNewCapa calls `rb_ary_new_capa` in C
-//
-// Original definition is following
-//
-//	VALUE rb_ary_new_capa(long capa)
-func RbAryNewCapa(capa int64) VALUE {
-	return VALUE(C.rb_ary_new_capa(C.long(capa)))
-}
-
 // RbAryNew2 is alias to [RbAryNewCapa]
 func RbAryNew2(capa int64) VALUE {
 	return RbAryNewCapa(capa)
-}
-
-// RbAryPush calls `rb_ary_push` in C
-//
-// Original definition is following
-//
-//	VALUE rb_ary_push(VALUE ary, VALUE elem)
-func RbAryPush(ary VALUE, elem VALUE) VALUE {
-	return VALUE(C.rb_ary_push(C.VALUE(ary), C.VALUE(elem)))
-}
-
-// RbAryPop calls `rb_ary_pop` in C
-//
-// Original definition is following
-//
-//	VALUE rb_ary_pop(VALUE ary)
-func RbAryPop(ary VALUE) VALUE {
-	return VALUE(C.rb_ary_pop(C.VALUE(ary)))
-}
-
-// RbAryShift calls `rb_ary_shift` in C
-//
-// Original definition is following
-//
-//	VALUE rb_ary_shift(VALUE ary)
-func RbAryShift(ary VALUE) VALUE {
-	return VALUE(C.rb_ary_shift(C.VALUE(ary)))
-}
-
-// RbAryUnshift calls `rb_ary_unshift` in C
-//
-// Original definition is following
-//
-//	VALUE rb_ary_unshift(VALUE ary, VALUE elem)
-func RbAryUnshift(ary VALUE, elem VALUE) VALUE {
-	return VALUE(C.rb_ary_unshift(C.VALUE(ary), C.VALUE(elem)))
 }
