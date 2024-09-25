b3a1689d1b65b63b52123d9896f6460201a1ece2
sue445 <sue445@sue445.net>
2024-09-25 23:21:55 +0900

    Remove legacy bindings

--- ruby/ruby_internal_arithmetic_int.go
+++ ruby/ruby_internal_arithmetic_int.go
@@ -7,30 +7,12 @@ import "C"
 
 // c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/arithmetic/int.h
 
-// RbNum2intInline calls `rb_num2int_inline` in C
-//
-// Original definition is following
-//
-//	int rb_num2int_inline(VALUE x)
-func RbNum2intInline(x VALUE) Int {
-	return Int(C.rb_num2int_inline(C.VALUE(x)))
-}
-
 // NUM2INT is alias to [RbNum2intInline]
 func NUM2INT(x VALUE) Int {
-	return RbNum2intInline(x)
-}
-
-// RbInt2numInline calls `rb_int2num_inline` in C
-//
-// Original definition is following
-//
-//	VALUE rb_int2num_inline(int v)
-func RbInt2numInline(v Int) VALUE {
-	return VALUE(C.rb_int2num_inline(C.int(v)))
+	return RbNum2IntInline(x)
 }
 
 // INT2NUM is alias to [RbInt2numInline]
 func INT2NUM(v Int) VALUE {
-	return RbInt2numInline(v)
+	return RbInt2NumInline(v)
 }
--- ruby/ruby_internal_arithmetic_long.go
+++ ruby/ruby_internal_arithmetic_long.go
@@ -7,30 +7,12 @@ import "C"
 
 // c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/arithmetic/long.h
 
-// RbNum2long calls `rb_num2long` in C
-//
-// Original definition is following
-//
-//	long rb_num2long(VALUE num)
-func RbNum2long(num VALUE) Long {
-	return Long(C.rb_num2long(C.VALUE(num)))
-}
-
 // NUM2LONG is alias to [RbNum2long]
 func NUM2LONG(num VALUE) Long {
-	return RbNum2long(num)
-}
-
-// RbLong2numInline calls `rb_long2num_inline` in C
-//
-// Original definition is following
-//
-//	VALUE rb_long2num_inline(long v)
-func RbLong2numInline(v Long) VALUE {
-	return VALUE(C.rb_long2num_inline(C.long(v)))
+	return RbNum2Long(num)
 }
 
 // LONG2NUM is alias to [RbLong2numInline]
 func LONG2NUM(v Long) VALUE {
-	return RbLong2numInline(v)
+	return RbLong2NumInline(v)
 }
