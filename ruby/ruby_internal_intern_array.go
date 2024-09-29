df7915c1068cd75ac6939e1e04cc745b0717e8db
sue445 <sue445@sue445.net>
2024-09-30 01:18:55 +0900

    Fixed cannot use capa (variable of type int64) as Long value in argument to RbAryNewCapa

--- ruby/ruby_internal_intern_array.go
+++ ruby/ruby_internal_intern_array.go
@@ -8,6 +8,6 @@ import "C"
 // c.f. https://github.com/ruby/ruby/blob/master/include/ruby/internal/intern/array.h
 
 // RbAryNew2 is alias to [RbAryNewCapa]
-func RbAryNew2(capa int64) VALUE {
+func RbAryNew2(capa Long) VALUE {
 	return RbAryNewCapa(capa)
 }
