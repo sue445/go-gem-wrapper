45b0139ba5fa0d794b6d9faa4c09d4dc3173363d
sue445 <sue445@sue445.net>
2024-10-05 14:36:34 +0900

    Migrate to autoload

--- _tools/ruby_h_to_go/lib/ruby_h_to_go.rb
+++ _tools/ruby_h_to_go/lib/ruby_h_to_go.rb
@@ -4,15 +4,14 @@ require "forwardable"
 
 require_relative "ruby_header_parser"
 
-require_relative "ruby_h_to_go/go_util"
-require_relative "ruby_h_to_go/argument_definition"
-require_relative "ruby_h_to_go/cli"
-require_relative "ruby_h_to_go/enum_definition"
-require_relative "ruby_h_to_go/function_definition"
-require_relative "ruby_h_to_go/struct_definition"
-require_relative "ruby_h_to_go/type_definition"
-require_relative "ruby_h_to_go/typeref_definition"
-
 # Generate Go binding from ruby.h
 module RubyHToGo
+  autoload :ArgumentDefinition, "ruby_h_to_go/argument_definition"
+  autoload :Cli,                "ruby_h_to_go/cli"
+  autoload :GoUtil,             "ruby_h_to_go/go_util"
+  autoload :EnumDefinition,     "ruby_h_to_go/enum_definition"
+  autoload :FunctionDefinition, "ruby_h_to_go/function_definition"
+  autoload :StructDefinition,   "ruby_h_to_go/struct_definition"
+  autoload :TypeDefinition,     "ruby_h_to_go/type_definition"
+  autoload :TyperefDefinition,  "ruby_h_to_go/typeref_definition"
 end
