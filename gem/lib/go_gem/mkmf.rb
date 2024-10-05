ecd104b9b3c00d4c4161b8c6eb26fbf0146328a1
sue445 <sue445@sue445.net>
2024-10-05 23:38:08 +0900

    path for go-gem

--- gem/lib/go_gem/mkmf.rb
+++ gem/lib/go_gem/mkmf.rb
@@ -8,7 +8,32 @@ module GoGem
     # @param target [String]
     # @param srcprefix [String,nil]
     def create_go_makefile(target, srcprefix = nil)
+      find_executable("go")
+
+      # rubocop:disable Style/GlobalVars
+      $objs = []
+      def $objs.empty?; false; end
+      # rubocop:enable Style/GlobalVars
+
       create_makefile(target, srcprefix)
+
+      case `#{CONFIG["CC"]} --version` # rubocop:disable Lint/LiteralAsCondition
+      when /Free Software Foundation/
+        ldflags = "-Wl,--unresolved-symbols=ignore-all"
+      when /clang/
+        ldflags = "-undefined dynamic_lookup"
+      end
+
+      current_dir = File.expand_path(".")
+
+      File.open("Makefile", "a") do |f|
+        f.write <<~MAKEFILE.gsub(/^ {8}/, "\t")
+          $(DLLIB): Makefile $(srcdir)/*.go
+                  cd $(srcdir); \
+                  CGO_CFLAGS='$(INCFLAGS)' CGO_LDFLAGS='#{ldflags}' \
+                    go build -p 4 -buildmode=c-shared -o #{current_dir}/$(DLLIB)
+        MAKEFILE
+      end
     end
   end
 end
